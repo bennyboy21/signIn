c0961381fe37e985ade089bfd8cebbf339fd1426
Benjamin Burnell <70352588+bennyboy21@users.noreply.github.com>
2023-10-30 08:07:14 -0400

    Add files via upload

---
+++ list.js
@@ -0,0 +1,123 @@
+const students_Names = [
+    "John Smith",
+    "Emma Johnson",
+    "Michael Williams",
+    "Olivia Brown",
+    "William Davis",
+    "Sophia Garcia",
+    "James Rodriguez",
+    "Emily Martinez",
+    "Alexander Hernandez",
+    "Charlotte Lopez",
+    "Daniel Gonzalez",
+    "Ava Wilson",
+    "Matthew Anderson",
+    "Amelia Moore",
+    "Joseph Taylor",
+    "Abigail Lewis",
+    "David Lee",
+    "Harper Hall",
+    "Andrew White",
+    "Ella Clark",
+    "Josephine Lewis",
+    "Gabriel Turner",
+    "Grace Scott",
+    "Nicholas Harris",
+    "Madison Young",
+    "Benjamin Walker",
+    "Elizabeth King",
+    "Samuel Turner",
+    "Lily Adams",
+    "Christopher Hill"
+];
+
+const students_Nums = [
+    123456789,
+    987654321,
+    234567890,
+    876543210,
+    345678901,
+    765432109,
+    456789012,
+    654321987,
+    567890123,
+    321098765,
+    678901234,
+    210987654,
+    789012345,
+    432109876,
+    543210987,
+    123098765,
+    789765432,
+    456123789,
+    987210345,
+    890654321,
+    109876543,
+    678432109,
+    567109823,
+    987654312,
+    890765432,
+    876543210,
+    987654321,
+    123456789,
+    234567890,
+    345678901
+];
+
+// const bibleTriviaQuestions = [
+//     {
+//         question: "In which city was Jesus born?",
+//         options: ["Jerusalem", "Nazareth", "Bethlehem", "Jericho"],
+//         correctAnswer: "Bethlehem"
+//     },
+//     {
+//         question: "Who is known as the 'Father of Faith' in the Bible?",
+//         options: ["Adam", "Moses", "Abraham", "David"],
+//         correctAnswer: "Abraham"
+//     },
+//     {
+//         question: "What is the last book of the New Testament?",
+//         options: ["Genesis", "Revelation", "Exodus", "Psalms"],
+//         correctAnswer: "Revelation"
+//     },
+//     {
+//         question: "Which disciple famously denied Jesus three times?",
+//         options: ["Peter", "John", "Thomas", "James"],
+//         correctAnswer: "Peter"
+//     },
+//     {
+//         question: "Who was thrown into a den of lions and survived in the Bible?",
+//         options: ["Jonah", "David", "Daniel", "Noah"],
+//         correctAnswer: "Daniel"
+//     },
+//     {
+//         question: "What type of wood was used to build Noah's Ark?",
+//         options: ["Oak", "Cedar", "Gopherwood", "Pine"],
+//         correctAnswer: "Gopherwood"
+//     },
+//     {
+//         question: "Which prophet was swallowed by a great fish for three days and nights?",
+//         options: ["Isaiah", "Jeremiah", "Ezekiel", "Jonah"],
+//         correctAnswer: "Jonah"
+//     },
+//     {
+//         question: "What did God create on the first day of creation in the Bible?",
+//         options: ["Sun", "Moon", "Light", "Earth"],
+//         correctAnswer: "Light"
+//     },
+//     {
+//         question: "Who was known for his coat of many colors in the Bible?",
+//         options: ["Solomon", "Jacob", "Joseph", "Abraham"],
+//         correctAnswer: "Jacob"
+//     },
+//     {
+//         question: "How many plagues did God send upon Egypt before the Exodus?",
+//         options: ["Five", "Seven", "Ten", "Twelve"],
+//         correctAnswer: "Ten"
+//     }
+// ];
+
+
+// const question1 = bibleTriviaQuestions[0].question;
+// const options1 = bibleTriviaQuestions[0].options;
+// const correctAnswer1 = bibleTriviaQuestions[0].correctAnswer;
